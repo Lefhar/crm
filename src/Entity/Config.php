88e6fe6cbb2f609718f249fbc6bfcc5b52e580b2
Lefebvre Harold <73162352+Lefhar@users.noreply.github.com>
2023-12-08 14:09:10 +0100

    modification type entité

--- src/Entity/Config.php
+++ src/Entity/Config.php
@@ -19,7 +19,7 @@ class Config
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: Types::TEXT)]
     private ?string $slogan = null;
 
     #[ORM\Column(length: 255)]
